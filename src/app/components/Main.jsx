75c8d5ac0ae680006b5fa38ec992e96c65db6f4d
Muhammad Noman <Muhammad.Noman@bentley.com>
2020-06-13 22:48:08 +0500

    Added router, routing and navigation

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {store} from '../store';
-
+import {Route, Router} from 'react-router-dom';
+import {history} from '../store/history';
 import {Dashboard, ConnectedDashboard} from './Dashboard';
+import {ConnectedNavigation} from './Navigation';
+
 export const Main = () =>(
+    <Router history={history}>
 <Provider store={store}>
 <div>
-    <ConnectedDashboard/>
+    <ConnectedNavigation/>
+    <Route exact path="/dashboard" render={()=>(<ConnectedDashboard/>)}>
+    
+    </Route>
 </div>
 </Provider>
-
+</Router>
 )
